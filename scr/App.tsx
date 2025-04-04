77270032744a9abe1b234d2d54a4a410f366ffc9
oleksandrgrebinskij <roki000sasha@gmail.com>
2025-04-04 17:29:01 +0300

    first commit

---
+++ scr/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View style={{flex: 1}}>
+     <Text>asd</Text>
+    </View>
+  );
+}
+export default App;
